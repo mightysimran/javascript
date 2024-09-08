80bd2a669c2275eff79bf9717cd224c81fb40541
mightysimran <mightysimran.codes@gmail.com>
2024-09-08 14:30:24 +0530

    Learn What Matters -> Done

---
+++ Learn What Matters/script.js
@@ -0,0 +1,59 @@
+// word vs keyword
+
+// difference between var, const and let
+
+// Hoisting - variables and functions are hoisted which means the interpreter moves their declaration at the top of their respective scope(s)
+
+// console.log(x) // returns undefined
+// console.log(y) // returns error - not defined
+// var x = 5;
+
+// Types - primitive and reference
+// reference = [] () {}
+// primitives = number, string, null, undefined, boolean
+
+// Conditionals
+
+// Loops - for, while
+
+// Functions
+
+// function f(x){ // Here x = parameter
+//     console.log(x);
+// }
+// let x = 25;
+// f(x); // Here x = argument
+
+// Arrays - An object that can store multiple values under a single variable name. These values can be of any data type — string, number, boolean, or any other data type(s)
+
+// var arr = [1,2,3,4,5];
+
+// push, pop, shift, unshift, splice
+// arr.push(6) // adds member at the end 
+// arr.pop() // removes member at the end
+// arr.unshift(0) // adds member at the front
+// arr.shift() // removes member at the front
+// arr.splice(f, s) // removes members within a specified range. first parameter, f = start index, second parameter, s = how many
+
+// Objects
+
+// empty
+// var x = {}
+
+// filled
+// var car = {
+//   brand: "",
+//   model: "",
+//   acceleration: "",
+//   power: "",
+//   misc: function () {},
+// };
+
+// How to access prop
+// console.log(car.acceleration);
+
+// How to add/modify value of prop
+// car.brand = "Aston Martin"
+
+// props - brand, model, acceleration, power
+// method - props whose values are functions
