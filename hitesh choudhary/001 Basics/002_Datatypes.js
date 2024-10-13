a1fb4fcfc704d49d74a5bea974ac787ee3f4f275
mightysimran <mightysimran.codes@gmail.com>
2024-10-13 20:24:22 +0530

    Datatypes

---
+++ hitesh choudhary/001 Basics/002_Datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; 
+
+// Primitive
+// number -> 1, 3.14, 25.596
+// bigint
+// string -> ""
+// symbol -> distinct
+// boolean -> true or false
+// null -> standalone value
+// undefined
+
+// Reference
+// [] () {}
+
+// console.log(typeof null) // -> object
+// console.log(typeof undefined) // -> undefined
+
+// Notes:
+// mdn web docs
+// https://tc39.es/
