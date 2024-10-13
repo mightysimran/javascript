3d3290616492b5c9a62d7b29cd14ecf789c5a43c
mightysimran <mightysimran.codes@gmail.com>
2024-10-13 19:07:13 +0530

    Variables - var, let and const

---
+++ hitesh choudhary/001 Basics/001_Variables.js
@@ -0,0 +1,12 @@
+const pi = 3.14
+let user = "anon"
+var location = "Earth"
+isHuman = true
+let something; // -> undefined
+
+// pi = 3 // -> Error
+user = "mightysimran"
+location = "The Himalayas"
+
+// console.log()
+console.table([pi, user, location, isHuman, something])
