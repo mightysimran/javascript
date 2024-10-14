e141ce394bf50dabbbca33d4e2af539a198d9e42
mightysimran <mightysimran.codes@gmail.com>
2024-10-14 19:38:26 +0530

    Conversion of datatypes

---
+++ hitesh choudhary/001 Basics/003_Conversions_and_Operations.js
@@ -0,0 +1,32 @@
+// let s = "25.6"
+
+// console.log(typeof s);
+// console.log(typeof(s));
+
+// let valAfterConversion = Number(s)
+// console.log(typeof valAfterConversion); // -> number
+// console.log(valAfterConversion); // -> NaN
+
+// Notes:
+// "5" -> 5
+// "25.596" -> 25.596
+// "2x" -> NaN
+// undefined -> NaN
+// null -> 0
+// true -> 1, false -> 0
+
+// let ZEN_MODE = "active"
+
+// let valAfterConversion = Boolean(ZEN_MODE)
+// console.log(valAfterConversion); // -> true
+
+// Notes:
+// 1 -> true, 0 -> false
+// "" -> false
+// "active" => true
+
+// let n = 25.596
+
+// let valAfterConversion = String(n)
+// console.log(valAfterConversion);
+// console.log(typeof valAfterConversion); // -> string
