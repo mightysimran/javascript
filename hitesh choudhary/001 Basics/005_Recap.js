4e263e7572786e2f03017f002d7fb05482d3fcb6
mightysimran <mightysimran.codes@gmail.com>
2024-10-15 19:37:31 +0530

    recap

---
+++ hitesh choudhary/001 Basics/005_Recap.js
@@ -0,0 +1,34 @@
+// Primitive
+// String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const PI = 3.14
+let n = 25.596
+let isHuman = true
+let temperature = null
+let email; // -> undefined
+
+const N = 425441265489413545451564454n;
+
+const str1 = Symbol('Hello, world.')
+const str2 = Symbol('Hello, world.')
+console.log(str1 === str2); // -> false
+
+// Reference (Non-primitive)
+// arrays, objects and functions
+
+rgb = ["Red", "Green", "Blue"]
+
+const star = {
+    name: "Sirius",
+    brightness: "25.4 times that of the Sun",
+};
+
+const fn = function () {
+    console.log("Hello, world.");
+}
+
+// console.log(typeof )
+
+// Reference:
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
