6e88aa23c7c76ad84c0c3affe171daaaa0257301
mightysimran <mightysimran.codes@gmail.com>
2024-10-17 00:44:17 +0530

    Date and Time

---
+++ hitesh choudhary/001 Basics/008_Date_and_Time.js
@@ -0,0 +1,30 @@
+// let now = new Date()
+
+// console.log(now);
+// console.log(now.toString());
+// console.log(now.toDateString());
+// console.log(now.toLocaleString());
+// console.log(now.toLocaleTimeString());
+
+// console.log(typeof now);
+
+// let random = new Date(2025, 0, 1) // -> 1/1/2025, 12:00:00 am
+// let random = new Date(2025, 0, 1, 5, 3) // -> 1/1/2025, 5:03:00 am
+// let random = new Date("2025-01-01") // -> 1/1/2025, 5:30:00 am
+// let random = new Date("01-01-2025") // -> 1/1/2025, 12:00:00 am
+// console.log(random.toLocaleString());
+
+// let timestamp = Date.now()
+// console.log(timestamp); // -> returns the number of milliseconds elapsed since the epoch
+// console.log(random.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// let today = new Date() 
+// console.log(today);
+// console.log(today.getMonth() + 1);
+// console.log(today.getDay());
+
+// console.log(today.toLocaleString('default', {weekday: "long",}))
+
+// Notes:
+// https://tc39.es/proposal-temporal/
